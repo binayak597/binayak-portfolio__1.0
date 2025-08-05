bec5b459c27c00b17a78566ff146325dd297ad1c
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-08-05 19:12:09 +0530

    integrate bento grid section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,9 +31,6 @@ const Hero = () => {
         />
         {/* Radial gradient for the container to give a faded look */}
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
-        {/* <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-          Backgrounds
-        </p> */}
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
