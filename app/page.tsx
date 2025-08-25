7a0c3dd3a66a2e410bc9e8f7f4281620f926f746
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-08-25 19:08:07 +0530

    add project section and improve navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 import { FaHome } from "react-icons/fa";
 
 
@@ -9,16 +11,11 @@ export default function Home() {
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
       <FloatingNav 
-      navItems={[
-        {
-          name: 'Home',
-          link: '/',
-          icon: <FaHome />
-        }
-      ]}
+      navItems={navItems}
       />
       <Hero />
       <Grid />
+      <RecentProject />
     </div>
    </main>
   );
